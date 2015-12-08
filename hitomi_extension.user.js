3e5a7cd0a57411c1a14f662632b36e70348e0a77
skygarlics <jewyon@naver.com>
2015-12-08 22:22:25 +0900

    timer added
    자동 페이지넘김 추가

--- hitomi_extension.user.js
+++ hitomi_extension.user.js
@@ -4,7 +4,7 @@
 // @description hitomi.la extension
 // @include     http://hitomi.la/reader/*
 // @include     https://hitomi.la/reader/*
-// @version     1
+// @version     1.01
 // @grant       none
 // ==/UserScript==
 
@@ -25,8 +25,75 @@ function keyPress(e) {
   } else if (e.keyCode == 69) {
     //alert('E pressed');
     unsafeWindow.prevPanel();
+  } else if (e.keyCode == 84) {
+    //alert('T pressed');
+    togglePager();
   }
 }
 
+function addPagerButton() {
+  var uol = document.getElementById("singlePage").parentNode.parentNode;
+  var lst = document.createElement("li");
+  var lnk = document.createElement("a");
+  var icn = document.createElement("i");
+
+  icn.setAttribute("class", "icon-time icon-white");
+
+  lnk.appendChild(icn);
+  lnk.title     = "t key";
+  lnk.id        = "autoPager";
+  lnk.innerHTML += " Auto pager";
+
+  lst.appendChild(lnk);
+  uol.appendChild(lst);
+}
+
+function addTimerBox() {
+  var uol = document.getElementById("single-page-select").parentNode.parentNode;
+  var lst = document.createElement("li");
+  var ipt = document.createElement("input");
+
+  ipt.type = "text";
+  ipt.id = "pageTimer";
+  ipt.style.width = "40px";
+  ipt.value = 0;
+
+  lst.appendChild(ipt);
+  lst.style.margin = "5px 0px";
+
+  uol.appendChild(lst);
+}
+
+function togglePager() {
+  var second = document.getElementById("pageTimer").value;
+  if (second < 1 || isNaN((second))) {
+    return
+  }
+
+  togglePager.flag = togglePager.flag? 0 : 1;
+  if (togglePager.flag) {
+    var pagerButton = document.getElementById("autoPager");
+    pagerButton.parentNode.classList.add("disable");
+    pagerButton.childNodes[0].classList.remove("icon-white");
+
+    togglePager.interval = setInterval(unsafeWindow.nextPanel, second*1000);
+
+  } else {
+    var pagerButton = document.getElementById("autoPager");
+    pagerButton.parentNode.classList.remove("disable");
+    pagerButton.childNodes[0].classList.add("icon-white");
+
+    clearInterval(togglePager.interval);
+  }
+}
+
+
+// event lister for shortcut
 document.addEventListener('keydown', keyPress);
 document.addEventListener('wheel', wheelFunction);
+
+
+// slideshow
+addPagerButton();
+document.getElementById("autoPager").addEventListener("click", togglePager);
+addTimerBox();
